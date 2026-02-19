380c9734807e6c0c96e606803e16582900e2d0d9
Elias.RS <elias.soto1984@gmail.com>
2026-02-19 11:38:11 -0300

    fix: correct import path for seoMeta in SchemaMarkup.jsx

--- src/Componentes/SchemaMarkup.jsx
+++ src/Componentes/SchemaMarkup.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
-import { ORGANIZATION } from '../../utils/seoMeta';
+import { ORGANIZATION } from '../utils/seoMeta';
 
 /**
  * 📋 Componente JSON-LD LocalBusiness + ProfessionalService
